f8096b2477f4bbe709ae668dd9921bce9f212abc
전동진 <dj24677@naver.com>
2021-04-27 19:17:41 +0900

    merong

---
+++ src/test/java/com/tsis/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsis.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
